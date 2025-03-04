54c9d15034c4f5d353f4c46e955a4cf30b96912b
Aleksey Myakinin <amykis@mail.ru>
2025-03-05 00:55:37 +0300

    Add item to an Array

---
+++ 8_kuy/Add_item_to_an_Array.py
@@ -0,0 +1,12 @@
+'''
+Description:
+Add the value "codewars" to the websites array.
+After your code executes the websites array should == ["codewars"]
+
+The websites array has already been defined for you using the following code:
+
+websites = []'''
+
+# add the value "codewars" to the already defined websites array
+
+websites.append("codewars")
