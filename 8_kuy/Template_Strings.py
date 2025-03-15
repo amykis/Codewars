df96fa11f032385c2126b77cd2dfbcb63a744966
Aleksey Myakinin <amykis@mail.ru>
2025-03-15 23:49:23 +0300

    Template Strings

---
+++ 8_kuy/Template_Strings.py
@@ -0,0 +1,18 @@
+'''
+Template Strings, this kata is mainly aimed at the new JS ES6 Update introducing Template Strings
+Task
+Your task is to return the correct string using the Template String Feature.
+Input
+Two Strings, no validation is needed.
+Output
+You must output a string containing the two strings with the word ```' are '```
+Reference: https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/template_strings
+'''
+
+def temple_strings(obj, feature):
+    # your code here
+    return f'{obj} are {feature}'
+    pass
+
+
+print(temple_strings("Animals","Good"))
